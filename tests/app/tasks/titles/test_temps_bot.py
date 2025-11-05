a9a8a9bf491f3dd2ab71a737c5947fc4c91a3893
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-06 02:31:39 +0300

    1

---
+++ tests/app/tasks/titles/test_temps_bot.py
@@ -0,0 +1,34 @@
+"""
+
+Tests
+
+"""
+
+from __future__ import annotations
+import pytest
+
+from src.app.tasks.titles.temps_bot import match_main_title, find_main_title, get_titles, get_files_list
+
+
+@pytest.mark.skip(reason="Pending write")
+def test_match_main_title(self):
+    # TODO: Implement test
+    pass
+
+
+@pytest.mark.skip(reason="Pending write")
+def test_find_main_title(self):
+    # TODO: Implement test
+    pass
+
+
+@pytest.mark.skip(reason="Pending write")
+def test_get_titles(self):
+    # TODO: Implement test
+    pass
+
+
+@pytest.mark.skip(reason="Pending write")
+def test_get_files_list(self):
+    # TODO: Implement test
+    pass
---
+++ tests/app/tasks/titles/test_titles_bot.py
@@ -0,0 +1,16 @@
+"""
+
+Tests
+
+"""
+
+from __future__ import annotations
+import pytest
+
+from src.app.tasks.titles.titles_bot import titles_task
+
+
+@pytest.mark.skip(reason="Pending write")
+def test_titles_task(self):
+    # TODO: Implement test
+    pass
