9e523ffc1fd98540729c573d856d9780892e090d
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-06 02:30:05 +0300

    1

---
+++ tests/app/tasks/injects/test_inject_tasks.py
@@ -0,0 +1,16 @@
+"""
+
+Tests
+
+"""
+
+from __future__ import annotations
+import pytest
+
+from src.app.tasks.injects.inject_tasks import inject_task
+
+
+@pytest.mark.skip(reason="Pending write")
+def test_inject_task(self):
+    # TODO: Implement test
+    pass
---
+++ tests/app/tasks/uploads/test_up.py
@@ -0,0 +1,22 @@
+"""
+
+Tests
+
+"""
+
+from __future__ import annotations
+import pytest
+
+from src.app.tasks.uploads.up import start_upload, upload_task
+
+
+@pytest.mark.skip(reason="Pending write")
+def test_start_upload(self):
+    # TODO: Implement test
+    pass
+
+
+@pytest.mark.skip(reason="Pending write")
+def test_upload_task(self):
+    # TODO: Implement test
+    pass
