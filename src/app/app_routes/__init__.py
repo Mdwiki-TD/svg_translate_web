6dc23cb8a774e2ccabd73a5433cfe06163c5d20d
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-10 05:21:31 +0300

    Update __init__.py

--- src/app/app_routes/__init__.py
+++ src/app/app_routes/__init__.py
@@ -1,10 +1,10 @@
 #
 from .auth.routes import bp_auth
-from .main.routes import bp_main
+from .admin.routes import bp_admin
 from .cancel_restart.routes import bp_tasks_managers
 from .explorer.routes import bp_explorer
-from .admin.routes import bp_admin
 from .fix_nested import bp_fix_nested, bp_fix_nested_explorer
+from .main.routes import bp_main
 from .tasks.routes import bp_tasks, close_task_store
 from .templates.routes import bp_templates
 
