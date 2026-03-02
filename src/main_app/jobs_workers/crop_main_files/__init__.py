de94ec74fc90f8247a64ad15e8e0785195b8aa64
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-02 04:40:32 +0300

    .

--- src/main_app/jobs_workers/crop_main_files/__init__.py
+++ src/main_app/jobs_workers/crop_main_files/__init__.py
@@ -2,7 +2,7 @@
 
 from .crop_file import crop_svg_file
 from .download import download_file_for_cropping
-from .process import process_crops
+from .process_new import process_crops
 from .upload import upload_cropped_file
 from .utils import generate_cropped_filename
 from .worker import crop_main_files_for_templates
