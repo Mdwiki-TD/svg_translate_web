82b17ace525d165fb55644ada90403b4097e5e1c
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-02 06:05:16 +0300

    Centralize pytest fixtures in conftest.py
    Move multiple realistic wikitext fixtures into tests/conftest.py and remove duplicate fixture definitions from individual test modules. Added fixtures: sample_from_prompt, sample_with_both_titles, sample_without_titles, sample_with_svglanguages_only, sample_multiple_owidslidersrcs. Cleaned up tests/main_app/tasks/titles/* and tests/main_app/utils/wikitext/temps_bot/* to use the shared fixtures to reduce duplication and improve test maintainability.

--- tests/main_app/tasks/titles/test_match_main_title.py
+++ tests/main_app/tasks/titles/test_match_main_title.py
@@ -21,64 +21,10 @@ from src.main_app.tasks.titles.utils.main_file import (
 # ---------- Fixtures with realistic wikitext samples ----------
 
 
-@pytest.fixture
-def sample_from_prompt() -> str:
-    """Sample wikitext similar to the user's prompt."""
-    return (
-        "*[[Commons:List of interactive graphs|Return to list]]\n"
-        "{{owidslider\n"
-        "|start        = 2022\n"
-        "|list         = Template:OWID/health expenditure government expenditure#gallery\n"
-        "|location     = commons\n"
-        "|caption      = \n"
-        "|title        = \n"
-        "|language     = \n"
-        "|file         = [[File:Health-expenditure-government-expenditure,World,2022 (cropped).svg|link=|thumb|upright=1.6|Health expenditure government expenditure]]\n"
-        "|startingView = World\n"
-        "}}\n"
-        '<syntaxhighlight lang="wikitext" style="overflow:auto;">\n'
-        "{{owidslider\n"
-        "|start        = 2022\n"
-        "|list         = Template:OWID/health expenditure government expenditure#gallery\n"
-        "|location     = commons\n"
-        "|caption      = \n"
-        "|title        = \n"
-        "|language     = \n"
-        "|file         = [[File:Health-expenditure-government-expenditure,World,2022 (cropped).svg|link=|thumb|upright=1.6|Health expenditure government expenditure]]\n"
-        "|startingView = World\n"
-        "}}\n"
-        "</syntaxhighlight>\n"
-        "*'''Source''': https://ourworldindata.org/grapher/health-expenditure-government-expenditure\n"
-        "*'''Translate''':  https://svgtranslate.toolforge.org/File:health-expenditure-government-expenditure,World,2000.svg\n"
-        "\n"
-        "==Data==\n"
-        "{{owidslidersrcs|id=gallery|widths=240|heights=240\n"
-        "|gallery-AllCountries=\n"
-        "File:health-expenditure-government-expenditure, 2000 to 2021, UKR.svg!country=UKR\n"
-        "File:health-expenditure-government-expenditure, 2002 to 2022, AFG.svg!country=AFG\n"
-        "File:health-expenditure-government-expenditure, 2000 to 2022, BGD.svg!country=BGD\n"
-        "File:health-expenditure-government-expenditure, 2000 to 2022, FSM.svg!country=FSM\n"
-        "File:health-expenditure-government-expenditure, 2000 to 2022, ERI.svg!country=ERI\n"
-        "File:health-expenditure-government-expenditure, 2017 to 2022, SSD.svg!country=SSD\n"
-        "File:health-expenditure-government-expenditure, 2013 to 2022, SOM.svg!country=SOM\n"
-        "File:health-expenditure-government-expenditure, 2000 to 2022, YEM.svg!country=YEM\n"
-        "}}\n"
-    )
 
 
-@pytest.fixture
-def sample_with_svglanguages_only() -> str:
-    """Wikitext with only SVGLanguages main title."""
-    return "{{SVGLanguages|parkinsons-disease-prevalence-ihme,World,1990.svg}}\n" "Some other text...\n"
 
 
-@pytest.fixture
-def sample_with_both_titles() -> str:
-    """Wikitext with both SVGLanguages and Translate line. SVGLanguages should take precedence."""
-    return (
-        "{{SVGLanguages|some_main_title,World,2010.svg}}\n"
-        "*'''Translate''': https://svgtranslate.toolforge.org/File:another-title,World,2005.svg\n"
-    )
 
 
 # ---------- Tests for match_main_title_from_url ----------
--- tests/main_app/tasks/titles/utils/test_find_main_title.py
+++ tests/main_app/tasks/titles/utils/test_find_main_title.py
@@ -12,40 +12,16 @@ import pytest
 from src.main_app.tasks.titles.utils.main_file import find_main_title
 
 
-@pytest.fixture
-def sample_with_svglanguages_only() -> str:
-    """Wikitext with only SVGLanguages main title."""
-    return "{{SVGLanguages|parkinsons-disease-prevalence-ihme,World,1990.svg}}\n" "Some other text...\n"
 
 
-@pytest.fixture
-def sample_with_both_titles() -> str:
-    """Wikitext with both SVGLanguages and Translate line. SVGLanguages should take precedence."""
-    return (
-        "{{SVGLanguages|some_main_title,World,2010.svg}}\n"
-        "*'''Translate''': https://svgtranslate.toolforge.org/File:another-title,World,2005.svg\n"
-    )
 
 
-@pytest.fixture
-def sample_without_titles() -> str:
-    """Wikitext lacking both SVGLanguages and Translate line."""
-    return "No main title here.\n{{owidslidersrcs|id=x|widths=100|heights=100|gallery-AllCountries=}}\n"
 
 
-@pytest.fixture
-def sample_multiple_owidslidersrcs() -> str:
-    """Wikitext containing multiple owidslidersrcs blocks and duplicate filenames."""
-    return (
-        "{{owidslidersrcs|id=a|widths=120|heights=120|gallery-AllCountries=\n"
-        "File:Alpha, 2000 to 2001, AAA.svg!country=AAA\n"
-        "File:Beta, 2001 to 2002, BBB.svg!country=BBB\n"
-        "}}\n"
-        "{{owidslidersrcs|id=b|widths=120|heights=120|gallery-AllCountries=\n"
-        "File:Beta, 2001 to 2002, BBB.svg!country=BBB\n"  # duplicate on purpose
-        "File:Gamma, 2002 to 2003, CCC.svg!country=CCC\n"
-        "}}\n"
-    )
+
+
+
+
 
 
 # ---------- Tests for find_main_title ----------
