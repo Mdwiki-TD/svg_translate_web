dcf908398454cdc752c1586acf8c306ae3d4715c
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-06 02:28:49 +0300

    .

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
-from . import svg_config, app, logger_config
+from . import svg_config, app, logger_config, main_app
 
 __all__ = [
+    "main_app",
     "app",
     "svg_config",
     "logger_config",
