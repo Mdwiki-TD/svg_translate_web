71a26527050ec68a62adfe7f4db061b208e3d5bc
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-06 02:14:09 +0300

    .

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+from . import svg_config, app, logger_config
+
+__all__ = [
+    "app",
+    "svg_config",
+    "logger_config",
+]
--- src/app.py
+++ src/app.py
@@ -6,14 +6,21 @@ WSGI entry point for SVG Translate.
 from __future__ import annotations
 import sys
 
-from svg_config import _env_file_path  # noqa: F401 # Triggers environment configuration
-from logger_config import configure_logging
+try:
+    from svg_config import _env_file_path  # noqa: F401 # Triggers environment configuration
+    from logger_config import configure_logging
+except ImportError:
+    from .svg_config import _env_file_path  # noqa: F401 # Triggers environment configuration
+    from .logger_config import configure_logging
 
 DEBUG = "debug" in sys.argv or "DEBUG" in sys.argv
 
 configure_logging(DEBUG)
 
-from main_app import create_app  # noqa: E402
+try:
+    from main_app import create_app  # noqa: E402
+except ImportError:
+    from .main_app import create_app  # noqa: E402
 
 app = create_app()
 
