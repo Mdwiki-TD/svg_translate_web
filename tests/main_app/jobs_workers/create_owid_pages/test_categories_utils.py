97a3a9f38df08c73aaf7ab97e46de6ad902094eb
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-17 02:31:56 +0300

    Update test_categories_utils.py

--- tests/main_app/jobs_workers/create_owid_pages/test_categories_utils.py
+++ tests/main_app/jobs_workers/create_owid_pages/test_categories_utils.py
@@ -9,6 +9,7 @@ from wikitextparser import WikiLink
 
 from src.main_app.jobs_workers.create_owid_pages.categories_utils import (
     _extract_categories,
+    extract_categories_list,
     extend_categories,
 )
 
@@ -20,6 +21,14 @@ class TestExtractCategories:
         """
         categories = _extract_categories(text)
         assert categories[0].string == WikiLink('[[Category:Category1]]').string
+
+
+class TestExtractCategoriesList:
+    def test_extract_categories_list(self):
+        old_categories = [WikiLink('[[Category:Category1]]')]
+        new_categories = [WikiLink('[[Category:Category2]]')]
+        categories_list = extract_categories_list(old_categories, new_categories)
+        assert categories_list == [WikiLink('[[Category:Category1]]'), WikiLink('[[Category:Category2]]')]
 
 
 class TestExtendCategories:
