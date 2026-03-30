d4371fb484205ebb6a763f412861c2e87458a066
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-30 07:46:02 +0300

    .

---
+++ tests/main_app/jobs_workers/add_svglanguages_template/test_add_svglanguages_template_utils.py
@@ -0,0 +1,38 @@
+
+from src.main_app.jobs_workers.add_svglanguages_template.utils import load_link, add_template_to_text
+
+
+def test_load_link():
+    wikitext = """
+*[[Commons:List of interactive graphs|Return to list]]
+
+{{owidslider
+|start        = 2021
+|list         = Template:OWID/share with mental and substance disorders#gallery
+|location      = commons
+|caption      =
+|title        =
+|language     =
+|file         = [[File:share with mental and substance disorders, World, 2021 (cropped).svg|link=|thumb|upright=1.6|share with mental and substance disorders]]
+|startingView = World
+}}
+<syntaxhighlight lang="wikitext" style="overflow:auto;">
+{{owidslider
+|start        = 2021
+|list         = Template:OWID/share with mental and substance disorders#gallery
+|location      = commons
+|caption      =
+|title        =
+|language     =
+|file         = [[File:share with mental and substance disorders, World, 2021 (cropped).svg|link=|thumb|upright=1.6|share with mental and substance disorders]]
+|startingView = World
+}}
+</syntaxhighlight>
+*'''Source''': https://ourworldindata.org/grapher/share-with-mental-and-substance-disorders
+*'''Translate''': https://svgtranslate.toolforge.org/File:share_with_mental_and_substance_disorders,_World,_1990.svg
+{{-}}
+    """
+
+    file_name = load_link(wikitext)
+    expected = "share_with_mental_and_substance_disorders,_World,_1990.svg"
+    assert file_name == expected
---
+++ tests/main_app/jobs_workers/add_svglanguages_template/test_add_svglanguages_template_worker.py
@@ -0,0 +1,4 @@
+"""
+"""
+
+from src.main_app.jobs_workers.add_svglanguages_template.worker import AddSvgSVGLanguagesTemplate
