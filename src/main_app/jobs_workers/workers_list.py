4352e187c50e02e0fa346aac707e957873e78a26
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-01 06:20:43 +0300

    .

---
+++ src/main_app/jobs_workers/workers_list.py
@@ -0,0 +1,45 @@
+from .add_svglanguages_template import add_svglanguages_template_to_templates
+from .collect_main_files_worker import collect_main_files_for_templates
+from .copy_svg_translation import copy_svg_translation_worker
+from .create_owid_pages import create_owid_pages_for_templates
+from .crop_main_files import crop_main_files_for_templates
+from .download_main_files_worker import download_main_files_for_templates
+from .fix_nested_main_files_worker import fix_nested_main_files_for_templates
+
+jobs_targets = {
+    "fix_nested_main_files": fix_nested_main_files_for_templates,
+    "collect_main_files": collect_main_files_for_templates,
+    "crop_main_files": crop_main_files_for_templates,
+    "create_owid_pages": create_owid_pages_for_templates,
+    "add_svglanguages_template": add_svglanguages_template_to_templates,
+    "download_main_files": download_main_files_for_templates,
+    "copy_svg_translation": copy_svg_translation_worker,
+}
+
+
+JOB_TYPE_TEMPLATES = {
+    "collect_main_files": "admins/jobs_templates/collect_main_files/details.html",
+    "crop_main_files": "admins/jobs_templates/crop_main_files/details.html",
+    "create_owid_pages": "admins/jobs_templates/create_owid_pages/details.html",
+    "fix_nested_main_files": "admins/jobs_templates/fix_nested_main_files/details.html",
+    "download_main_files": "admins/jobs_templates/download_main_files/details.html",
+    "add_svglanguages_template": "admins/jobs_templates/add_svglanguages_template/details.html",
+    "copy_svg_translation": "admins/jobs_templates/copy_svg_translation/details.html",
+}
+
+
+JOB_TYPE_LIST_TEMPLATES = {
+    "collect_main_files": "admins/jobs_templates/collect_main_files/list.html",
+    "crop_main_files": "admins/jobs_templates/crop_main_files/list.html",
+    "create_owid_pages": "admins/jobs_templates/create_owid_pages/list.html",
+    "fix_nested_main_files": "admins/jobs_templates/fix_nested_main_files/list.html",
+    "download_main_files": "admins/jobs_templates/download_main_files/list.html",
+    "add_svglanguages_template": "admins/jobs_templates/add_svglanguages_template/list.html",
+    "copy_svg_translation": "admins/jobs_templates/copy_svg_translation/list.html",
+}
+
+__all__ = [
+    "jobs_targets",
+    "JOB_TYPE_TEMPLATES",
+    "JOB_TYPE_LIST_TEMPLATES",
+]
