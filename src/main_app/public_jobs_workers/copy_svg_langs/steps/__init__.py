023007bbc5ce03688a0d501e174a658396142b5b
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-02 01:30:26 +0300

    .

--- src/main_app/public_jobs_workers/copy_svg_langs/steps/__init__.py
+++ src/main_app/public_jobs_workers/copy_svg_langs/steps/__init__.py
@@ -1,11 +1,11 @@
 from ..utils import make_results_summary, save_files_stats
-from .download import download_step
-from .extract_text import extract_text_step
-from .extract_titles import extract_titles_step
-from .extract_translations import extract_translations_step
-from .fix_nested import fix_nested_step
-from .inject import inject_step
-from .upload import upload_step
+from ...copy_svg_langs_legacy.steps import download_step
+from ...copy_svg_langs_legacy.steps import extract_text_step
+from ...copy_svg_langs_legacy.steps import extract_titles_step
+from ...copy_svg_langs_legacy.steps import extract_translations_step
+from ...copy_svg_langs_legacy.steps import fix_nested_step
+from ...copy_svg_langs_legacy.steps import inject_step
+from ...copy_svg_langs_legacy.steps import upload_step
 
 __all__ = [
     "download_step",
