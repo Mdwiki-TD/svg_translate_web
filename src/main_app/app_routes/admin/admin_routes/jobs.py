43d37a498c721356c47bbb3399162938c1316a9f
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-01 06:21:20 +0300

    Update jobs.py

--- src/main_app/app_routes/admin/admin_routes/jobs.py
+++ src/main_app/app_routes/admin/admin_routes/jobs.py
@@ -18,6 +18,8 @@ from flask import (
 from flask.typing import ResponseReturnValue
 from werkzeug.wrappers.response import Response
 
+from ....jobs_workers.workers_list import JOB_TYPE_LIST_TEMPLATES, JOB_TYPE_TEMPLATES
+
 from ....admins.admins_required import admin_required
 from ....config import settings
 from ....jobs_workers import jobs_worker
@@ -28,25 +30,6 @@ from ...utils.routes_utils import load_auth_payload
 
 logger = logging.getLogger(__name__)
 
-
-JOB_TYPE_TEMPLATES = {
-    "collect_main_files": "admins/jobs_templates/collect_main_files/details.html",
-    "crop_main_files": "admins/jobs_templates/crop_main_files/details.html",
-    "create_owid_pages": "admins/jobs_templates/create_owid_pages/details.html",
-    "add_svglanguages_template": "admins/jobs_templates/add_svglanguages_template/details.html",
-    "fix_nested_main_files": "admins/jobs_templates/fix_nested_main_files/details.html",
-    "download_main_files": "admins/jobs_templates/download_main_files/details.html",
-}
-
-
-JOB_TYPE_LIST_TEMPLATES = {
-    "collect_main_files": "admins/jobs_templates/collect_main_files/list.html",
-    "crop_main_files": "admins/jobs_templates/crop_main_files/list.html",
-    "create_owid_pages": "admins/jobs_templates/create_owid_pages/list.html",
-    "add_svglanguages_template": "admins/jobs_templates/add_svglanguages_template/list.html",
-    "fix_nested_main_files": "admins/jobs_templates/fix_nested_main_files/list.html",
-    "download_main_files": "admins/jobs_templates/download_main_files/list.html",
-}
 
 
 def _cancel_job(job_id: int, job_type: str) -> Response:
