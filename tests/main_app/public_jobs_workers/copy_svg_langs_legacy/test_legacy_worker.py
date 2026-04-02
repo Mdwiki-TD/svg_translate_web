846beb5ea072735ed23f46132bee854d660ad0f1
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-02 06:43:37 +0300

    Update test_legacy_worker.py

--- tests/main_app/public_jobs_workers/copy_svg_langs_legacy/test_legacy_worker.py
+++ tests/main_app/public_jobs_workers/copy_svg_langs_legacy/test_legacy_worker.py
@@ -2,6 +2,5 @@ from pathlib import Path
 from unittest.mock import MagicMock, patch
 
 from src.main_app.public_jobs_workers.copy_svg_langs_legacy.worker import (
-    CopySvgLangsWorker,
     copy_svg_langs_worker_entry,
 )
