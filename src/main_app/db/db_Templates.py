cd6a2a0c4ee8cdd1eeb7d0b98cb17ac4aa0e7e52
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-06 02:54:46 +0300

    Update db_Templates.py

--- src/main_app/db/db_Templates.py
+++ src/main_app/db/db_Templates.py
@@ -186,7 +186,9 @@ class TemplatesDB:
 
             if field in strip_fields:
                 value = _strip_file_prefix(value)
-            value = value.strip()
+
+            value = value.strip() if isinstance(value, str) else value
+
             _data[field] = value
             add_fields.append(field)
             add_values.append(value)
