643a6b5139289241c7445046dc667f7218121137
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-07 02:44:31 +0300

    .

--- src/main_app/app_routes/admin_routes/owid_charts.py
+++ src/main_app/app_routes/admin_routes/owid_charts.py
@@ -19,7 +19,6 @@ from flask.typing import ResponseReturnValue
 
 from ...db import OwidChartRecord
 from ...services import owid_charts_service
-from ...services.users_service import current_user
 from ..admin.admins_required import admin_required
 
 logger = logging.getLogger(__name__)
