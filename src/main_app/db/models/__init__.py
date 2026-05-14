b0246878f9dc87ee40874e737b3f3ff2b9cdd4cb
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-05-14 07:10:56 +0300

    Update __init__.py

--- src/main_app/db/models/__init__.py
+++ src/main_app/db/models/__init__.py
@@ -4,7 +4,6 @@ from .settings import SettingRecord
 from .templates import TemplateRecord
 from .users import AdminUserRecord, UserTokenRecord
 
-
 __all__ = [
     "UserTokenRecord",
     "AdminUserRecord",
