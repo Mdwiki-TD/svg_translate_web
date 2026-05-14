7cd2eda6ace4dccb251fe01b9f5f3d3b6105f109
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-05-14 07:08:23 +0300

    Update api_routes.py

--- src/main_app/app_routes/api_routes.py
+++ src/main_app/app_routes/api_routes.py
@@ -4,13 +4,12 @@ import logging
 
 from flask import Blueprint, jsonify, request
 
-from ..sqlalchemy_db.services import list_templates_need_update
-
 from ..db.models import OwidChartRecord, TemplateRecord
 from ..db.services import (
     owid_charts_service,
     template_service,
 )
+from ..sqlalchemy_db.services import list_templates_need_update
 
 logger = logging.getLogger(__name__)
 
