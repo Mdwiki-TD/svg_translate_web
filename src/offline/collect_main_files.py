83b3476d693c54029873e8107b19646669f175f8
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-05-15 04:38:07 +0300

    Update collect_main_files.py

--- src/offline/collect_main_files.py
+++ src/offline/collect_main_files.py
@@ -24,10 +24,10 @@ from src.main_app.api_services import get_category_members, get_wikitext
 from src.main_app.jobs_workers.base_worker import BaseJobWorker
 from src.main_app.live_db.services import (
     add_template_data,
-    list_templates,
-    update_template_data,
     create_job,
+    list_templates,
     update_job_status,
+    update_template_data,
 )
 from src.main_app.utils.wikitext import find_template_source
 from src.main_app.utils.wikitext.titles_utils import (
