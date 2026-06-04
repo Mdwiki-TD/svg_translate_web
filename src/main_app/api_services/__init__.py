48221b0a4084dbc03c86258361a3317b67cc45c9
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-04 05:33:36 +0300

    Update __init__.py

--- src/main_app/api_services/__init__.py
+++ src/main_app/api_services/__init__.py
@@ -3,12 +3,42 @@
 from .category import get_category_members
 from .clients import get_user_site
 from .clients.commons_client import create_commons_session, download_commons_file_core
-from .query_api import get_wikitext
+from .pages_api import (
+    create_page,
+    get_file_text,
+    get_page_text,
+    is_page_exists,
+    is_redirect,
+    move_page,
+    update_file_text,
+    update_page_text,
+)
+from .query_api import (
+    get_double_redirects,
+    get_page_links,
+    get_template_pages,
+    is_pages_exists,
+    resolve_redirects,
+    search_pages,
+)
 
 __all__ = [
     "create_commons_session",
     "download_commons_file_core",
     "get_user_site",
     "get_category_members",
-    "get_wikitext",
+    "get_template_pages",
+    "get_double_redirects",
+    "get_page_links",
+    "is_pages_exists",
+    "resolve_redirects",
+    "search_pages",
+    "create_page",
+    "get_page_text",
+    "is_page_exists",
+    "is_redirect",
+    "move_page",
+    "update_page_text",
+    "update_file_text",
+    "get_file_text",
 ]
