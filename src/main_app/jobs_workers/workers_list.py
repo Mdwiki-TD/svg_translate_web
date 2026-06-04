c0efd9d25f1c620c9228f7bad3f0b53aee39ec16
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-04 04:42:59 +0300

    .

--- src/main_app/jobs_workers/workers_list.py
+++ src/main_app/jobs_workers/workers_list.py
@@ -31,7 +31,7 @@ jobs_data = {
         job_list_template="admins/jobs_templates/collect_main_files/list.html",
         job_callable=collect_main_files_for_templates,
         job_args=[],
-        start_confirm_message="",
+        start_confirm_message="This will start a background job to collect templates data for all templates that don't have one. Continue?",
     ),
     "update_owid_charts": JobData(
         job_type="update_owid_charts",
@@ -40,7 +40,7 @@ jobs_data = {
         job_list_template="admins/jobs_templates/update_owid_charts/list.html",
         job_callable=update_owid_charts_worker_entry,
         job_args=["owid_charts_limit_items"],
-        start_confirm_message="",
+        start_confirm_message="This will fetch metadata from ourworldindata.org for every chart and update min_time / max_time / len_years where changed. Continue?",
     ),
     "crop_main_files": JobData(
         job_type="crop_main_files",
@@ -49,7 +49,7 @@ jobs_data = {
         job_list_template="admins/jobs_templates/crop_main_files/list.html",
         job_callable=crop_main_files_for_templates,
         job_args=["crop_newest_upload_limit"],
-        start_confirm_message="",
+        start_confirm_message="This will start a background job to crop newest world files and upload them with '(cropped)' suffix. Continue?",
     ),
     "fix_nested_main_files": JobData(
         job_type="fix_nested_main_files",
@@ -58,7 +58,7 @@ jobs_data = {
         job_list_template="admins/jobs_templates/fix_nested_main_files/list.html",
         job_callable=fix_nested_main_files_for_templates,
         job_args=[],
-        start_confirm_message="",
+        start_confirm_message="This will start a background job to fix nested tags in all template main files. This will upload fixed versions to Commons using your credentials. Continue?",
     ),
     "create_owid_pages": JobData(
         job_type="create_owid_pages",
@@ -67,7 +67,7 @@ jobs_data = {
         job_list_template="admins/jobs_templates/create_owid_pages/list.html",
         job_callable=create_owid_pages_for_templates,
         job_args=["create_owid_pages_limit"],
-        start_confirm_message="",
+        start_confirm_message="This will start a background job to create showcase pages for OWID templates. Continue?",
     ),
     "rename_owid_pages": JobData(
         job_type="rename_owid_pages",
@@ -76,7 +76,7 @@ jobs_data = {
         job_list_template="admins/jobs_templates/rename_owid_pages/list.html",
         job_callable=rename_owid_pages_for_templates,
         job_args=[],
-        start_confirm_message="",
+        start_confirm_message='This will start a background job that renames every Template:OWID/* and OWID/* page whose first character after "OWID/" is lowercase. Continue?',
     ),
     "add_svglanguages_template": JobData(
         job_type="add_svglanguages_template",
@@ -85,7 +85,7 @@ jobs_data = {
         job_list_template="admins/jobs_templates/add_svglanguages_template/list.html",
         job_callable=add_svglanguages_template_to_templates,
         job_args=["add_svglanguages_limit_items"],
-        start_confirm_message="",
+        start_confirm_message="This will start a background job to add Template:SVGLanguages to OWID templates.\nContinue?",
     ),
     "download_main_files": JobData(
         job_type="download_main_files",
@@ -94,7 +94,7 @@ jobs_data = {
         job_list_template="admins/jobs_templates/download_main_files/list.html",
         job_callable=download_main_files_for_templates,
         job_args=["download_main_files_limit_items"],
-        start_confirm_message="",
+        start_confirm_message="This will start a background job to download all main files from the remote source to the local filesystem. Continue?",
     ),
 }
 
