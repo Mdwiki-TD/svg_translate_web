dc4ed66b55d78cd602d3450ef54acecbc716c85c
ibrahem Qasim <ibrahemqasim@wikiprojectmed.org>
2026-06-06 08:58:26 +0300

    Update cli_jobs.py

--- src/main_app/jobs_workers/cli_jobs.py
+++ src/main_app/jobs_workers/cli_jobs.py
@@ -6,16 +6,16 @@ import logging
 
 from flask import Flask
 
-from .jobs_worker import start_job_cli
+from .jobs_worker import start_job
 
 logger = logging.getLogger(__name__)
 
 def register_cli_jobs(app: Flask) -> None:
     @app.cli.command("run-collect-templates-data")
     def _run() -> None:
-        start_job_cli(
+        start_job(
             user={"username": "Background job"},
             job_type="collect_templates_data",
             args={"update_all":"true"},
-            app=app,
+            daemon=False,
         )
