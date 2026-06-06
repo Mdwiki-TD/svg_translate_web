eef05eccdfdf58492270ecf498aa0691a3c96a1d
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-07 01:54:38 +0300

    .

--- src/main_app/jobs_workers/cli_jobs.py
+++ src/main_app/jobs_workers/cli_jobs.py
@@ -6,16 +6,35 @@ import logging
 
 from flask import Flask
 
-from .jobs_worker import start_job
+from .jobs_worker import start_job_cli
 
 logger = logging.getLogger(__name__)
 
 def register_cli_jobs(app: Flask) -> None:
     @app.cli.command("run-collect-templates-data")
     def _run() -> None:
-        start_job(
+        """
+        how to test locally:
+        .venv/Scripts/activate
+        flask --app src/app1.py run-collect-templates-data
+        """
+        start_job_cli(
             user={"username": "Background job"},
             job_type="collect_templates_data",
             args={"update_all":"true"},
-            daemon=False,
+            app=app,
+        )
+
+    @app.cli.command("collect-templates")
+    def _run_not_update() -> None:
+        """
+        how to test locally:
+        .venv/Scripts/activate
+        flask --app src/app1.py collect-templates
+        """
+        start_job_cli(
+            user={"username": "Background job"},
+            job_type="collect_templates_data",
+            args={"update_all":"true"},
+            app=app,
         )
