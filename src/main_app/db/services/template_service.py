4e6aa7946ae415817a521962547363890e3fcde1
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-06 06:55:59 +0300

    .

--- src/main_app/db/services/template_service.py
+++ src/main_app/db/services/template_service.py
@@ -6,6 +6,7 @@ from typing import Any, List
 from ...extensions import db
 from ...utils.wikitext.titles_utils import match_last_world_year
 from ..models.templates import TemplateRecord
+from .utils import db_retry, db_guard
 
 logger = logging.getLogger(__name__)
 
@@ -70,6 +71,8 @@ def add_template_data(
     return chart
 
 
+# @db_retry(default_return=None, msg="Failed to update template", max_retries=3, retry_delay=2.0)
+@db_guard(default_return=None)
 def update_template_data(
     template_id: int,
     template_data: dict[str, str],
