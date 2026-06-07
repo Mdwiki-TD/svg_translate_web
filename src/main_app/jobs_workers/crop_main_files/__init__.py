47a6ef66cb80d94de58b8e19e5c4d36a777e4e4e
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-07 05:12:56 +0300

    Update __init__.py

--- src/main_app/jobs_workers/crop_main_files/__init__.py
+++ src/main_app/jobs_workers/crop_main_files/__init__.py
@@ -4,13 +4,12 @@ from .crop_file import crop_svg_file
 from .crop_utils import generate_cropped_filename
 from .download import download_file_for_cropping
 from .upload import upload_cropped_file
-from .worker import crop_main_files_worker_entry, process_crops
+from .worker import crop_main_files_worker_entry
 
 __all__ = [
     "crop_main_files_worker_entry",
     "generate_cropped_filename",
     "download_file_for_cropping",
     "upload_cropped_file",
-    "process_crops",
     "crop_svg_file",
 ]
