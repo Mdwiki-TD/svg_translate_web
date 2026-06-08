6f5660f90e79ab22955eefd3079c40f6d44392ad
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-09 02:55:19 +0300

    Create test_services_utils.py

---
+++ tests/unit/db/services/test_services_utils.py
@@ -0,0 +1,12 @@
+"""
+Unit tests for src/main_app/db/services/utils.py module.
+
+Functions to test: db_guard_rollback, db_guard
+
+TODO: write tests
+"""
+
+
+from src.main_app.db.services.utils import (
+    db_guard_rollback, db_guard,
+)
