0722a5de620062537e4eed3d37ae003a48a66e2f
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-09 02:55:27 +0300

    Create test_users.py

---
+++ tests/unit/db/models/test_users.py
@@ -0,0 +1,12 @@
+"""
+Unit tests for src/main_app/db/models/users.py module.
+
+Classes to test: UsersRecord, AdminUserRecord, UserTokenRecord
+
+TODO: write tests
+"""
+
+
+from src.main_app.db.models.users import (
+    UsersRecord, AdminUserRecord, UserTokenRecord,
+)
