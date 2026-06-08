96b1abc57b0c9a7d90bf4007d909798bb97cfae7
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-08 04:26:03 +0300

    .

--- src/main_app/public_jobs_workers/workers_list_public.py
+++ src/main_app/public_jobs_workers/workers_list_public.py
@@ -6,8 +6,8 @@ jobs_data_public = {
     "copy_svg_langs": JobData(
         job_type="copy_svg_langs",
         job_name="Copy SVG Translation",
-        job_details_template="jobs_templates/copy_svg_langs/details.html",
-        job_list_template="jobs_templates/copy_svg_langs/list.html",
+        job_details_template="jobs_templates/public/copy_svg_langs/details.html",
+        job_list_template="jobs_templates/public/copy_svg_langs/list.html",
         job_callable=copy_svg_langs_worker_entry,
         job_args=["copy_svg_langs_upload_limit"],
         start_confirm_message="",
@@ -15,8 +15,8 @@ jobs_data_public = {
     "fix_nested_jobs": JobData(
         job_type="fix_nested_jobs",
         job_name="Fix Nested Tasks",
-        job_details_template="jobs_templates/fix_nested_jobs/details.html",
-        job_list_template="jobs_templates/fix_nested_jobs/list.html",
+        job_details_template="jobs_templates/public/fix_nested_jobs/details.html",
+        job_list_template="jobs_templates/public/fix_nested_jobs/list.html",
         job_callable=fix_nested_jobs_worker_entry,
         job_args=[],
         start_confirm_message="",
