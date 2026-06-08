01bfc074f9f74fcae4e9ba8f7ef66e92750f8503
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-09 02:55:41 +0300

    Create test_template_need_update_service.py

---
+++ tests/unit/db/services/test_template_need_update_service.py
@@ -0,0 +1,12 @@
+"""
+Unit tests for src/main_app/db/services/template_need_update_service.py module.
+
+Functions to test: list_templates_need_update
+
+TODO: write tests
+"""
+
+
+from src.main_app.db.services.template_need_update_service import (
+    list_templates_need_update,
+)
