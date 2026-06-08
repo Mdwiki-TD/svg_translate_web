fac38136fab98fb15e6ea2c2ad6b42cf9e29374a
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-09 02:56:25 +0300

    Create test_coordinators.py

---
+++ tests/unit/app_routes/admin_routes/test_coordinators.py
@@ -0,0 +1,11 @@
+"""
+Unit tests for src/main_app/app_routes/admin_routes/coordinators.py module.
+
+Classes to test: CoordinatorsRoutes
+
+TODO: write tests
+"""
+
+from src.main_app.app_routes.admin_routes.coordinators import (
+    CoordinatorsRoutes,
+)
