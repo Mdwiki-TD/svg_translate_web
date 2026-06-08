b36edf9cc7e872c0d59a50ccbfab6a71363dca69
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-09 02:57:01 +0300

    Create test_flask_config.py

---
+++ tests/unit/config/test_flask_config.py
@@ -0,0 +1,16 @@
+"""
+Unit tests for src/main_app/config/flask_config.py module.
+
+Classes to test: Config, DevelopmentConfig, ProductionConfig, TestingConfig
+Functions to test: build_sqlalchemy_uri
+
+TODO: write tests
+"""
+
+from src.main_app.config.flask_config import (
+    Config,
+    DevelopmentConfig,
+    ProductionConfig,
+    TestingConfig,
+    build_sqlalchemy_uri,
+)
