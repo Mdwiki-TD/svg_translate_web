0dc1bb3554222fd4214376ea2a666bb5e0c8b875
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-09 02:55:31 +0300

    Create test_owid_slug_redirects.py

---
+++ tests/unit/db/models/test_owid_slug_redirects.py
@@ -0,0 +1,12 @@
+"""
+Unit tests for src/main_app/db/models/owid_slug_redirects.py module.
+
+Classes to test: OwidSlugRedirectRecord
+
+TODO: write tests
+"""
+
+
+from src.main_app.db.models.owid_slug_redirects import (
+    OwidSlugRedirectRecord,
+)
