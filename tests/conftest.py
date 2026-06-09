f851b3439e4db863c0d2ea15552e0bf9e1b978a6
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-09 06:53:09 +0300

    .

--- tests/conftest.py
+++ tests/conftest.py
@@ -48,6 +48,7 @@ def stop_nets(request):
     # Check if 'network' mark is present in the current test item
     if "network" in request.node.keywords:
         from pytest_socket import enable_socket
+
         enable_socket()
         return
     # Otherwise, disable the socket for all other tests
