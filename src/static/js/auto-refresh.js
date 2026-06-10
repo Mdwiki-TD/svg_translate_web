b7d882dc1448d39efbca052c836ae29b41e3763c
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-10 08:50:49 +0300

    Update auto-refresh.js

--- src/static/js/auto-refresh.js
+++ src/static/js/auto-refresh.js
@@ -1,18 +1,35 @@
 let _refreshTimer = null;
 let _autoRefreshActive = false;
 
-function toggleAutoRefresh() {
+function startAutoRefresh() {
+    _autoRefreshActive = true;
     const indicator = document.getElementById('auto-refresh-indicator');
+    if (indicator) indicator.classList.remove('d-none');
+    _refreshTimer = setInterval(function () {
+        location.reload();
+    }, 5000);
+}
+
+function stopAutoRefresh() {
+    clearInterval(_refreshTimer);
+    _refreshTimer = null;
+    _autoRefreshActive = false;
+    const indicator = document.getElementById('auto-refresh-indicator');
+    if (indicator) indicator.classList.add('d-none');
+}
+
+function toggleAutoRefresh() {
     if (_autoRefreshActive) {
-        clearInterval(_refreshTimer);
-        _refreshTimer = null;
-        _autoRefreshActive = false;
-        if (indicator) indicator.classList.add('d-none');
+        stopAutoRefresh();
+        history.replaceState(null, '', window.location.pathname + window.location.search);
     } else {
-        _autoRefreshActive = true;
-        if (indicator) indicator.classList.remove('d-none');
-        _refreshTimer = setInterval(function () {
-            location.reload();
-        }, 5000);
+        location.hash = 'AutoRefresh';
+        startAutoRefresh();
     }
 }
+
+(function () {
+    if (window.location.hash === '#AutoRefresh') {
+        startAutoRefresh();
+    }
+})();
