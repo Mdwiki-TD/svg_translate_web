7288eabf678db234e38577ceefa7c880d5cc106f
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-11 05:22:02 +0300

    Update settings.py

--- src/main_app/db/models/settings.py
+++ src/main_app/db/models/settings.py
@@ -2,35 +2,46 @@ from __future__ import annotations
 
 import logging
 
-from sqlalchemy import Column, Enum, Integer, String, Text
+from enum import StrEnum
+from sqlalchemy import String, Text, text
+from sqlalchemy.orm import Mapped, mapped_column
 
 from ...extensions import db
 
 logger = logging.getLogger(__name__)
 
+class ValueType(StrEnum):
+    BOOLEAN = "boolean"
+    STRING = "string"
+    INTEGER = "integer"
+
 
 class SettingRecord(db.Model):
     """
     CREATE TABLE `settings` (
-      `id` int(11) NOT NULL AUTO_INCREMENT,
-      `key` varchar(190) NOT NULL,
-      `title` varchar(500) NOT NULL,
-      `value` text DEFAULT NULL,
-      `value_type` enum('boolean','string','integer') NOT NULL DEFAULT 'boolean',
-      PRIMARY KEY (`id`),
-      UNIQUE KEY `unique_key` (`key`)
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `key` varchar(190) NOT NULL,
+        `title` varchar(500) NOT NULL,
+        `value` text DEFAULT NULL,
+        `value_type` enum('boolean','string','integer') NOT NULL DEFAULT 'boolean',
+        PRIMARY KEY (`id`),
+        UNIQUE KEY `unique_key` (`key`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
     """
 
     __tablename__ = "settings"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    key = Column(String(190), unique=True, nullable=False)
-    title = Column(String(500), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    key: Mapped[str] = mapped_column(String(190), unique=True, nullable=False)
+    title: Mapped[str] = mapped_column(String(500), nullable=False)
 
-    value_type = Column(Enum("boolean", "string", "integer"), nullable=False, server_default="boolean")
+    value_type: Mapped[ValueType] = mapped_column(
+        nullable=False,
+        server_default=text("'boolean'")
+    )
 
-    value = Column(Text, nullable=True)
+    value: Mapped[str | None] = mapped_column(Text, nullable=True)
+
 
 
 __all__ = [
