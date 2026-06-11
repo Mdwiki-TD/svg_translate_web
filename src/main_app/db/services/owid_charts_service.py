a2ab11426f15e61f87a8186c3968e41e0a9b25ca
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-11 06:20:01 +0300

    Update owid_charts_service.py

--- src/main_app/db/services/owid_charts_service.py
+++ src/main_app/db/services/owid_charts_service.py
@@ -3,13 +3,9 @@ from __future__ import annotations
 import logging
 from typing import Any, Optional
 
-from sqlalchemy.orm import joinedload
-
 from ...extensions import db
 from ..models.owid_charts import OwidChartRecord
 from .utils import db_guard, db_guard_rollback
-
-# from ..models.views import OwidChartTemplateRecord
 
 logger = logging.getLogger(__name__)
 
@@ -23,16 +19,10 @@ def list_charts(limit: int | None = None) -> list[OwidChartRecord]:
         WHERE oct.chart_id = oc.chart_id
         ORDER BY oc.chart_id ASC
     """
-    query = (
-        db.session.query(OwidChartRecord)
-        .options(joinedload(OwidChartRecord._template_info))
-        .order_by(OwidChartRecord.chart_id.asc())
-    )
+    query = db.session.query(OwidChartRecord).order_by(OwidChartRecord.chart_id.asc())
     if limit is not None:
         query = query.limit(limit)
 
-    # owid_charts_service.py:33: SAWarning: Multiple rows returned with uselist=False for
-    # eagerly-loaded attribute 'OwidChartRecord._template_info'
     return query.all()
 
 
@@ -49,14 +39,13 @@ def list_published_charts() -> list[OwidChartRecord]:
     query = (
         db.session.query(OwidChartRecord)
         .filter(OwidChartRecord.is_published == 1)
-        .options(joinedload(OwidChartRecord._template_info))
         .order_by(OwidChartRecord.chart_id.asc())
     )
     records = query.all()
     return records
 
 
-def get_chart_by_id(chart_id: int) -> OwidChartRecord:
+def get_chart_by_id(chart_id: int) -> OwidChartRecord | None:
     """
     Fetch a single chart by ID.
 
@@ -65,12 +54,8 @@ def get_chart_by_id(chart_id: int) -> OwidChartRecord:
         WHERE oct.chart_id = %s
         and oct.chart_id = oc.chart_id
     """
-    return (
-        db.session.query(OwidChartRecord)
-        .filter(OwidChartRecord.chart_id == chart_id)
-        .options(joinedload(OwidChartRecord._template_info))
-        .first()
-    )
+    records = db.session.query(OwidChartRecord).filter(OwidChartRecord.chart_id == chart_id).first()
+    return records
 
 
 def get_chart_by_slug(slug: str) -> Optional[OwidChartRecord]:
@@ -82,11 +67,7 @@ def get_chart_by_slug(slug: str) -> Optional[OwidChartRecord]:
         WHERE oc.slug = %s
         and oct.chart_id = oc.chart_id
     """
-    return (
-        db.session.query(OwidChartRecord).filter(OwidChartRecord.slug == slug)
-        # .options(joinedload(OwidChartRecord._template_info))
-        .first()
-    )
+    return db.session.query(OwidChartRecord).filter(OwidChartRecord.slug == slug).first()
 
 
 @db_guard_rollback
@@ -96,14 +77,6 @@ def add_chart(
     """
     Add a new chart.
     """
-    chart_data.update(
-        {
-            "has_map_tab": 1 if chart_data.get("has_map_tab") else 0,
-            "is_published": 1 if chart_data.get("is_published") else 0,
-            "single_year_data": 1 if chart_data.get("single_year_data") else 0,
-            "has_timeline": 1 if chart_data.get("has_timeline") else 0,
-        }
-    )
     chart_data = {
         key: value for key, value in chart_data.items() if value is not None and hasattr(OwidChartRecord, key)
     }
