8bbbfe46becc44690fd0fcc88653e52e918e8375
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-07-06 09:48:05 +0300

    Update worker.py

--- src/main_app/jobs_workers/admin_jobs_workers/update_owid_charts/worker.py
+++ src/main_app/jobs_workers/admin_jobs_workers/update_owid_charts/worker.py
@@ -207,6 +207,7 @@ class UpdateOwidChartsWorker(BaseObjectsJobWorker):
                     "skip_reason": "not found",
                 }
             )
+            _ = self._update(chart, {"status": 404}, info)
             return False
 
         if metadata is None:
