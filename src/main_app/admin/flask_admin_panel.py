e9b9d30c7e22cad7be5cae1d8d2d384f3c0804e3
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-08-01 04:03:00 +0300

    .

--- src/main_app/admin/flask_admin_panel.py
+++ src/main_app/admin/flask_admin_panel.py
@@ -1,7 +1,7 @@
 """Admin blueprint package."""
 
 from flask import Flask
-from flask_admin import Admin#, BaseView, expose
+from flask_admin import Admin, AdminIndexView#, BaseView, expose
 from flask_admin.contrib.sqla import ModelView
 from flask_admin.theme import Bootstrap4Theme
 from flask_babel import Babel
@@ -9,30 +9,34 @@ from flask_babel import Babel
 from ..db.models import (
     AdminUserRecord,
     JobRecord,
+    UserRecord,
+    # UserTokenRecord,
     OwidChartRecord,
     OwidSlugRedirectRecord,
     SettingRecord,
-    TemplateNeedUpdateView,
     TemplateRecord,
-    UserRecord,
+    # TemplateNeedUpdateView,
 )
 
 
 class WrapModelView(ModelView):
-    form_excluded_columns = ('created_at', 'token')
+    ignore_hidden = True
+    form_excluded_columns = ('created_at', 'updated_at', 'token')
     column_display_actions: bool = True
     action_disallowed_list = ['delete']
     page_size: int = 50
     # edit_modal: bool = True
     # create_modal: bool = True
+    can_edit: bool = True
+    can_delete: bool = False
     can_view_details: bool = True
 
 def add_admin_dashboard(app: Flask, _db) -> None:
-    babel = Babel(app)
+    babel = Babel(app) # pyright: ignore
     # Initialize Admin and add views
     theme = Bootstrap4Theme(
-        base_template="admin/index.html",
-        swatch="cerulean",
+        base_template="admin/index_with_sidebar.html",
+        swatch="default",
         fluid=True,
     )
     admin = Admin(
@@ -40,38 +44,44 @@ def add_admin_dashboard(app: Flask, _db) -> None:
         name="DB admin",
         theme=theme,
         endpoint=None,
+        index_view=AdminIndexView(
+            name='DB admin',
+            template='admin/index_with_sidebar.html',
+            url='/adminpanel/db_admin'
+        )
     )
 
     admin.add_category(
-        name="Main",
+        name="Templates",
         class_name=None,
-        icon_type=None,
-        icon_value=None,
+        icon_type="fa",
+        icon_value="fa-cubes",
     )
 
-    main_models = [
-        WrapModelView(TemplateRecord, _db, category="Main"),
-        WrapModelView(TemplateNeedUpdateView, _db, name="Templates Need Update", category="Main"),
-        WrapModelView(OwidChartRecord, _db, category="Main", name="OWID Charts"),
-        WrapModelView(OwidSlugRedirectRecord, _db, category="Main"),
-    ]
-    admin.add_views(*main_models)
+    admin.add_category(
+        name="OwidCharts",
+        class_name=None,
+        icon_type="fa",
+        icon_value="fa-cubes",
+    )
 
     admin.add_category(
         name="Users",
         class_name=None,
-        icon_type=None,
-        icon_value=None,
+        icon_type="fa",
+        icon_value="fa-users",
     )
 
-    users_models = [
+    all_models = [
+        WrapModelView(TemplateRecord, _db, category="Templates"),
+        # WrapModelView(TemplateNeedUpdateView, _db, name="Templates Need Update", category="Templates"),
+
+        WrapModelView(OwidChartRecord, _db, name="OwidChartRecord", category="OwidCharts"),
+        WrapModelView(OwidSlugRedirectRecord, _db, name="OwidSlugRedirectRecord", category="OwidCharts"),
+
         WrapModelView(AdminUserRecord, _db, category="Users"),
         WrapModelView(UserRecord, _db, category="Users"),
-    ]
 
-    admin.add_views(*users_models)
-
-    all_models = [
         WrapModelView(JobRecord, _db),
         WrapModelView(SettingRecord, _db),
     ]
