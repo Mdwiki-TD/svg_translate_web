627938ec68b25b131a3ff6119008645ad8fce4d1
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-08-08 04:28:55 +0300

    Update objects.py

--- src/main_app/jobs_workers/objects.py
+++ src/main_app/jobs_workers/objects.py
@@ -1,7 +1,6 @@
 from collections.abc import Callable
 from dataclasses import dataclass, field
 
-
 @dataclass
 class JobData:
     job_type: str
@@ -13,6 +12,7 @@ class JobData:
     job_args: list[dict[str, str]] = field(default_factory=list)
     start_confirm_message: str | None = None
     load_settings: bool = False
+    form_class: Callable | None = None
 
 
 __all__ = [
