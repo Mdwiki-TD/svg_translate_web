dbc6674e318a7593ac347be04b58d754d8b5f550
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-09-21 05:42:24 +0300

    .

--- src/main_app/admin/admin_panel.py
+++ src/main_app/admin/admin_panel.py
@@ -29,6 +29,7 @@ class AdminPanel:
 
     def register(self, bp: Blueprint) -> None:
         bp.app_context_processor(self.inject_sidebar)
+        # TODO: put a before_request guard on the admin blueprint. use admin_required decorators
 
         routes = [
             ("/", "GET", self.admin_dashboard),
